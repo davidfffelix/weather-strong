dc85de19cefe1f80ae2f724ce120f65d4de8aac4
David Felix <davidffelix@hotmail.com>
2022-04-07 22:45:36 -0300

    Edit event

--- script/script.js
+++ script/script.js
@@ -3,6 +3,9 @@ inputPart = wrapper.querySelector(".input-part");
 infoTxt = inputPart.querySelector(".info-txt");
 inputField = inputPart.querySelector("input");
 
-inputField.addEventListener("keyup", e =>) {
-    if()
-}
+inputField.addEventListener("keyup", e => {
+    // If user pressed Enter btn and input value is not empty
+    if(e.key == "Enter" && inputField.value != ""){
+        console.log("Hello")
+    }          
+})
